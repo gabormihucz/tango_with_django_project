d1294bfe9057634404e950cdde46149c944efc36
magnarch <magnarch@gmail.com>
2018-02-06 20:45:23 +0000

    Chapter 8 with exercises + half of Chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 from rango.models import Category, Page
 
+from rango.models import UserProfile # user auth - chap9
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
@@ -14,3 +16,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
